41f6078547614d69e53734b36b216f89ac622be4
Eric <evyrfgp@gmail.com>
2021-10-08 16:21:42 +0400

    init

---
+++ fibonacci_numbers_cash.py
@@ -0,0 +1,27 @@
+import time
+
+
+def cash(func):
+    def wrapper(number, cash_data={}):
+        current_el = cash_data.get(number)
+        if not current_el:
+            current_el = func(number)
+            cash_data.update({number: current_el})
+        return current_el
+
+    return wrapper
+
+
+@cash
+def get_fibonacci_numbers(number):
+    # quadratic complexity
+    if number in [1, 2]:
+        return 1
+    else:
+        return get_fibonacci_numbers(number - 1) + get_fibonacci_numbers(number - 2)
+
+
+start_time = time.time()
+print(get_fibonacci_numbers(51))
+end_time = time.time()
+print(f'time result {end_time - start_time}')
