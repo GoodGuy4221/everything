b17ec5c0ef7fea5b57d7809139a68f227065f1d7
Eric <evyrfgp@gmail.com>
2021-10-08 16:36:23 +0400

    fast fibonacci

--- fibonacci_numbers_cash.py
+++ fibonacci_numbers_cash.py
@@ -1,12 +1,24 @@
 import time
 
 
+# def cash(func):
+#     def wrapper(number, cash_data={1: 1, 2: 1}):
+#         current_el = cash_data.get(number)
+#         if not current_el:
+#             current_el = func(number)
+#             cash_data.update({number: current_el})
+#         return current_el
+#
+#     return wrapper
+
+
 def cash(func):
-    def wrapper(number, cash_data={}):
-        current_el = cash_data.get(number)
-        if not current_el:
+    def wrapper(number, cash_data=[1, 1]):
+        if len(cash_data) <= number:
             current_el = func(number)
-            cash_data.update({number: current_el})
+            cash_data.append(current_el)
+        else:
+            current_el = cash_data[number]
         return current_el
 
     return wrapper
@@ -22,6 +34,6 @@ def get_fibonacci_numbers(number):
 
 
 start_time = time.time()
-print(get_fibonacci_numbers(51))
+print(get_fibonacci_numbers(480))
 end_time = time.time()
 print(f'time result {end_time - start_time}')
