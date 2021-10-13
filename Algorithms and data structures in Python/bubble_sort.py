d806a7da8c0e5e55a0254192d3fd0edae33ec799
Eric <evyrfgp@gmail.com>
2021-10-13 09:07:07 +0400

    _

--- Algorithms and data structures in Python/bubble_sort.py
+++ Algorithms and data structures in Python/bubble_sort.py
@@ -1,11 +1,8 @@
-import random
-
 test_array = [4, 5, 2, 6, 7, 3, 1, 8, 9, 0]
 
-n = 1
-while n < len(test_array):
+# O(n**2)
+for _ in test_array:
     for i in range(len(test_array) - 1):
         if test_array[i] > test_array[i + 1]:
             test_array[i], test_array[i + 1] = test_array[i + 1], test_array[i]
-    n += 1
     print(test_array)
