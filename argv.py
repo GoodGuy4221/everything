3abb76c2b2d133d1c1f21e43e2fdb50fb79fb8b5
Eric <evyrfgp@gmail.com>
2021-10-21 14:41:19 +0400

    _

--- argv.py
+++ argv.py
@@ -1,8 +1,22 @@
 import sys
+import argparse
+
+
+def show_user(name, *args):
+    message = [f'name user {name}']
+    if args:
+        message.append(f'age {args[0]}')
+    print(', '.join(message))
+
 
 # print(sys.argv)
-app, *args = sys.argv
-print(app, *args, sep='\n')
+# app, *args = sys.argv
+# print(app, *args, sep='\n')
 
-# args = sys.argv[:1]
+args = sys.argv[1:]
 # print(args)
+if args:
+    show_user(*args)
+else:
+    name = input('enter name ')
+    show_user(name)
