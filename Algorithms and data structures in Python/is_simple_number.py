4d1db09416e5d01b47e97f7eaa589e4b0f224575
Eric <evyrfgp@gmail.com>
2021-10-25 12:22:34 +0400

    _

--- Algorithms and data structures in Python/is_simple_number.py
+++ Algorithms and data structures in Python/is_simple_number.py
@@ -1,4 +1,4 @@
-def is_simple_number(n: (int, float)) -> bool:
+def is_simple_number(n: int) -> bool:
     if n == 1:
         return False
     i = 2
@@ -9,7 +9,19 @@ def is_simple_number(n: (int, float)) -> bool:
     return True
 
 
+def eratosthenes(number: int):
+    n = list(range(number + 1))
+    n[0] = n[1] = False
+    for i in range(2, number):
+        if n[i]:
+            for j in range(2 * i, number + 1, i):
+                n[j] = False
+    return [i for i in n if i]
+
+
 if __name__ == '__main__':
     print(is_simple_number(51))
     print(is_simple_number(52))
-    print(is_simple_number(52))
+    print(is_simple_number(2))
+    print(eratosthenes(2))
+    print(eratosthenes(50))
